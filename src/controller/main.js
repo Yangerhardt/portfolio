38b20a52560850fb70a2ad36a299d501bb851aed
Yan - PC Lajeado <yan-ragna@hotmail.com>
2022-11-20 10:30:40 -0300

    Melhorando o css

--- src/controller/main.js
+++ src/controller/main.js
@@ -2,25 +2,20 @@ import darkFunction from "../service/darkFunction.js";
 import ingles from "../service/ingles.js";
 import portugues from "../service/portugues.js";
 
-const darkMode = document.querySelector("#dark-mode")
+const darkMode = document.querySelector("#dark-mode");
 let pt_br = document.querySelector("#btn-br");
 let en_us = document.querySelector("#btn-en");
 let c = 0;
 
-window.addEventListener("DOMContentLoaded", () => {
-
-})
-
 darkMode.addEventListener("click", () => {
-  darkFunction(c)
+  darkFunction(c);
   c++;
-})
-
+});
 
 pt_br.addEventListener("click", () => {
-  portugues()
+  portugues();
 });
 
 en_us.addEventListener("click", () => {
-  ingles()
+  ingles();
 });
